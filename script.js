90db16bd7fdedb520565b5fd5c84896f3271af9d
pratikdake <130586960+pratikdake@users.noreply.github.com>
2023-09-29 21:55:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write somethng");
+
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+
+}
+
